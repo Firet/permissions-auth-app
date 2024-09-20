1017985e8799427236d4aff890a0093b0c47f40b
Firet <damianfederico@protonmail.com>
2024-09-20 19:18:47 -0300

    Create layout.tsx and move Layout component from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,14 @@ import * as React from "react";
 import {
   Routes,
   Route,
-  Link,
   useNavigate,
   useLocation,
   Navigate,
-  Outlet,
 } from "react-router-dom";
 import { fakeAuthProvider } from "./auth";
 import PublicPage from "./pages/PublicPage";
 import ProtectedPage from "./pages/ProtectedPage";
+import Layout from "./layout";
 
 export default function App() {
   return (
@@ -30,25 +29,6 @@ export default function App() {
         </Route>
       </Routes>
     </AuthProvider>
-  );
-}
-
-function Layout() {
-  return (
-    <div>
-      <AuthStatus />
-
-      <ul>
-        <li>
-          <Link to="/">Public Page</Link>
-        </li>
-        <li>
-          <Link to="/protected">Protected Page</Link>
-        </li>
-      </ul>
-
-      <Outlet />
-    </div>
   );
 }
 
@@ -86,7 +66,7 @@ function useAuth() {
   return React.useContext(AuthContext);
 }
 
-function AuthStatus() {
+export function AuthStatus() {
   let auth = useAuth();
   let navigate = useNavigate();
 
