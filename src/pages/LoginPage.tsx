c70a219df1a1b4546651d31b3accf382f9ddfb72
Firet <damianfederico@protonmail.com>
2024-09-21 00:18:36 -0300

    Redirect to public after succesful login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,36 +5,36 @@ export default function LoginPage() {
     let navigate = useNavigate();
     let location = useLocation();
     let auth = useAuth();
-  
+
     let from = location.state?.from?.pathname || "/";
-  
+
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-      event.preventDefault();
-  
-      let formData = new FormData(event.currentTarget);
-      let username = formData.get("username") as string;
-  
-      auth.signin(username, () => {
-        // Send them back to the page they tried to visit when they were
-        // redirected to the login page. Use { replace: true } so we don't create
-        // another entry in the history stack for the login page.  This means that
-        // when they get to the protected page and click the back button, they
-        // won't end up back on the login page, which is also really nice for the
-        // user experience.
-        navigate(from, { replace: true });
-      });
+        event.preventDefault();
+
+        let formData = new FormData(event.currentTarget);
+        let username = formData.get("username") as string;
+
+        auth.signin(username, () => {
+            navigate('/public', { replace: true });
+        });
+
+        // auth.admin(() => {
+        //     navigate('/protected', { replace: true });
+        // });
     }
-  
+
     return (
-      <div>
-        <p>You must log in to view the page at {from}</p>
-  
-        <form onSubmit={handleSubmit}>
-          <label>
-            Username: <input name="username" type="text" />
-          </label>{" "}
-          <button type="submit">Login</button>
-        </form>
-      </div>
+        <div>
+
+            {auth.user && <h3>auth user is: {auth.user}</h3>}
+            <p>You must log in to view the page at {from}</p>
+
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username: <input name="username" type="text" />
+                </label>{" "}
+                <button type="submit">Login</button>
+            </form>
+        </div>
     );
-  }
+}
