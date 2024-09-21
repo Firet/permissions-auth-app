3798e648e8bcdb155a42ace6c1cb30ca8eb30046
Firet <damianfederico@protonmail.com>
2024-09-21 20:44:50 -0300

    Add min-length and max-length error messages for username and password input

--- src/components/CustomForm.tsx
+++ src/components/CustomForm.tsx
@@ -1,11 +1,12 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
+// import { ErrorMessage } from '@hookform/error-message'
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../App";
-
 
 interface IFormInput {
     userName: string
     password: string
+    multipleErrorInput: string
 }
 
 
@@ -21,24 +22,43 @@ export default function CustomForm() {
             });
         }
     })
-    console.log('errors', errors);
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <label>Username: </label>
             <input
-                {...register("userName", { required: true, minLength: 4, maxLength: 20 })}
+                {...register("userName", {
+                    required: 'Username is required',
+                    minLength: { value: 4, message: "Must exceed 3 characters" },
+                    maxLength: { value: 21, message: "Must have less than 20 characters" }
+                })}
                 aria-invalid={errors.userName ? "true" : "false"}
             />
             {errors.userName?.type === "required" && (
-                <p role="alert">Username is required</p>
+                <p role="alert">{errors.userName.message}</p>
+            )}
+            {errors.userName?.type === "minLength" && (
+                <p role="alert">{errors.userName.message}</p>
+            )}
+            {errors.userName?.type === "maxLength" && (
+                <p role="alert">{errors.userName.message}</p>
             )}
             <label>Password: </label>
             <input
-                {...register("password", { required: true, minLength: 4, maxLength: 20 })}
-                aria-invalid={errors.password ? "true" : "false"}
+                {...register("password", {
+                    required: 'Password is required',
+                    minLength: { value: 4, message: "Must exceed 3 characters" },
+                    maxLength: { value: 21, message: "Must have less than 20 characters" }
+                })}
+                aria-invalid={errors.userName ? "true" : "false"}
             />
             {errors.password?.type === "required" && (
-                <p role="alert">Password is required</p>
+                <p role="alert">{errors.password.message}</p>
+            )}
+            {errors.password?.type === "minLength" && (
+                <p role="alert">{errors.password.message}</p>
+            )}
+            {errors.password?.type === "maxLength" && (
+                <p role="alert">{errors.password.message}</p>
             )}
             <button type="submit">Log In</button>
         </form>
