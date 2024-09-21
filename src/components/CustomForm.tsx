1407fb2cc6826d59070156a211018713d1f8b69f
Firet <damianfederico@protonmail.com>
2024-09-21 20:13:47 -0300

    Add requiered error messages for username and password input

--- src/components/CustomForm.tsx
+++ src/components/CustomForm.tsx
@@ -12,7 +12,7 @@ interface IFormInput {
 export default function CustomForm() {
     let navigate = useNavigate();
     let auth = useAuth();
-    const { register, handleSubmit } = useForm<IFormInput>();
+    const { register, formState: { errors }, handleSubmit } = useForm<IFormInput>();
 
     const onSubmit: SubmitHandler<IFormInput> = (data => {
         if (data.userName) {
@@ -21,13 +21,25 @@ export default function CustomForm() {
             });
         }
     })
-
+    console.log('errors', errors);
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <label>Username: </label>
-            <input {...register("userName", { required: true, minLength: 4, maxLength: 20 })} />
+            <input
+                {...register("userName", { required: true, minLength: 4, maxLength: 20 })}
+                aria-invalid={errors.userName ? "true" : "false"}
+            />
+            {errors.userName?.type === "required" && (
+                <p role="alert">Username is required</p>
+            )}
             <label>Password: </label>
-            <input {...register("password", { required: true, minLength: 4, maxLength: 20 })} />
+            <input
+                {...register("password", { required: true, minLength: 4, maxLength: 20 })}
+                aria-invalid={errors.password ? "true" : "false"}
+            />
+            {errors.password?.type === "required" && (
+                <p role="alert">Password is required</p>
+            )}
             <button type="submit">Log In</button>
         </form>
     )
