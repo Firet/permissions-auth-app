43e933763e47b7b63211d91b7dbf370b0c0ee1c3
Firet <damianfederico@protonmail.com>
2024-09-21 17:17:40 -0300

    Add authState type

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ export default function App() {
 }
 
 interface AuthContextType {
-  authState: any,
+  authState: { isAuthenticated: boolean, isAdmin: boolean },
   user: string | null;
   signin: (user: string, callback: VoidFunction) => void;
   signout: (callback: VoidFunction) => void;
