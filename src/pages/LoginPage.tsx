cbdb298c72e1fad6712af7cfe79524f3d5d5a157
Firet <damianfederico@protonmail.com>
2024-09-21 23:27:17 -0300

    Center CustomForm in LoginPage using Emotion Styled

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,10 +2,20 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../App";
 import { useEffect } from "react";
 import CustomForm from "../components/CustomForm";
+import styled from '@emotion/styled';
 
 export default function LoginPage() {
     let navigate = useNavigate();
     let auth = useAuth();
+
+    const FormContainer = styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%; 
+        height: 100vh;
+`;
 
     useEffect(() => {
         if (auth.user) {
@@ -14,9 +24,9 @@ export default function LoginPage() {
     }, [auth.user, navigate]);
 
     return (
-        <div>
+        <FormContainer>
             <h3>Login Page</h3>
             <CustomForm />
-        </div>
+        </FormContainer>
     );
 }
