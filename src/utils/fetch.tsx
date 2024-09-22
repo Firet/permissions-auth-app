58020435cd3b57006e2881b276a7eb7226e38525
Firet <damianfederico@protonmail.com>
2024-09-22 00:33:29 -0300

    Create fetch.tsx and post.tsx to conect with jsonplaceholder

---
+++ src/utils/fetch.tsx
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export default function fetchData(data: any) {
+	const url = `https://jsonplaceholder.typicode.com/users`;
+	// const fakeData = {
+	// 	method: 'GET',
+	// 	body: JSON.stringify({
+	// 		title: 'foo',
+	// 		body: 'bar',
+	// 		userId: 1,
+	// 	}),
+	// 	headers: {
+	// 		'Content-type': 'application/json; charset=UTF-8',
+	// 	},
+	// };
+	return axios.get(url, data)
+        .then((response) => console.log(response))
+		.then(() => console.info("Hook sent correctly"))
+		.catch(() => console.warn("Hook failed"))
+}
---
+++ src/utils/post.tsx
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default function post(data: any) {
+	const url = `https://jsonplaceholder.typicode.com/posts`;
+	// const fakeData = {
+	// 	method: 'POST',
+	// 	body: JSON.stringify({
+	// 		title: 'foo',
+	// 		body: 'bar',
+	// 		userId: 1,
+	// 	}),
+	// 	headers: {
+	// 		'Content-type': 'application/json; charset=UTF-8',
+	// 	},
+	// };
+	return axios.post(url, data)
+		.then(() => console.info("Hook sent correctly"))
+		.catch(() => console.warn("Hook failed"))
+}
