f3944dfd7e1996b3dad286bd5a4e380c0772f82e
Firet <damianfederico@protonmail.com>
2024-09-21 22:20:26 -0300

    Create material UI theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 import './index.css';
 import App from './App';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
