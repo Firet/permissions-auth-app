aec8d4f055978daf7a6daad088d58f7dbe3af514
Firet <damianfederico@protonmail.com>
2024-09-25 09:06:54 -0300

    Change Home text and colors

--- src/components/pages/PublicPage.tsx
+++ src/components/pages/PublicPage.tsx
@@ -4,21 +4,34 @@ export default function PublicPage() {
 	const HomeContainer = styled.div`
 		display: flex;
 		flex-direction: column;
-		align-items: center;
 		justify-content: flex-start;
 		margin-top: 50px;
+		margin-left:20%
 		width: 100%;
 		min-height: 100vh;
 		font-weight: 600;
+		border: 2px solid var(--primary);
+		padding: 2rem;
+	`;
+
+	const HomeTitle = styled.h3`
+		text-align: center;
+	`;
+
+	const HomeText = styled.p`
+		margin-top: 2rem;
+	`;
+
+	const SpanText = styled.span`
+		background-color: var(--primary);
 	`;
 
 	return (
 		<>
-			<h3>Home</h3>
 			<HomeContainer>
-				<p>This is an authentication app</p>
-				<p>Click on Log In button and choose any username to enter as a User Role</p>
-				<p>But you can login with the username 'admin' and password 'admin' to enter as an Administrator role</p>
+				<HomeTitle>This is an authentication Demo</HomeTitle>
+				<HomeText>Log In with any username to enter as a <SpanText>User Role</SpanText></HomeText>
+				<HomeText>Otherwise, you can login with the username 'admin' and password 'admin' to enter as an <SpanText>Administrator role</SpanText></HomeText>
 			</HomeContainer>
 		</>
 	);
