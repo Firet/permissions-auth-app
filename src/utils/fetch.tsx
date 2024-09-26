7d3a5422f9ce3fcac0fc46a95e733551db53f9f0
Firet <damianfederico@protonmail.com>
2024-09-26 09:01:20 -0300

    Remove commented code in fetch and post functions

--- src/utils/fetch.tsx
+++ src/utils/fetch.tsx
@@ -2,17 +2,6 @@ import axios from 'axios';
 
 export default function fetchData(data: any) {
 	const url = `https://jsonplaceholder.typicode.com/users`;
-	// const fakeData = {
-	// 	method: 'GET',
-	// 	body: JSON.stringify({
-	// 		title: 'foo',
-	// 		body: 'bar',
-	// 		userId: 1,
-	// 	}),
-	// 	headers: {
-	// 		'Content-type': 'application/json; charset=UTF-8',
-	// 	},
-	// };
 	return axios
 		.get(url, data)
 		.then((response) => console.log(response))
--- src/utils/post.tsx
+++ src/utils/post.tsx
@@ -2,17 +2,6 @@ import axios from 'axios';
 
 export default function post(data: any) {
 	const url = `https://jsonplaceholder.typicode.com/posts`;
-	// const fakeData = {
-	// 	method: 'POST',
-	// 	body: JSON.stringify({
-	// 		title: 'foo',
-	// 		body: 'bar',
-	// 		userId: 1,
-	// 	}),
-	// 	headers: {
-	// 		'Content-type': 'application/json; charset=UTF-8',
-	// 	},
-	// };
 	return axios
 		.post(url, data)
 		.then(() => console.info('Hook sent correctly'))
